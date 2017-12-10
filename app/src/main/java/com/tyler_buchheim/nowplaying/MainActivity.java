90e0d099cc0dfda2b341d0f910879b9633d5e989
Tyler Buchheim <tbuchhei@alumni.nd.edu>
2017-12-09 18:08:41 -0600

    Initial commit

---
+++ app/src/main/java/com/tyler_buchheim/nowplaying/MainActivity.java
@@ -0,0 +1,13 @@
+package com.tyler_buchheim.nowplaying;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
